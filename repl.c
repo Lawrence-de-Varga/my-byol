7ee085cea5cc27b48d5330baf7f21d7a22390729
Lawrence de Varga <lawdevarga@gmail.com>
2023-01-07 21:20:24 +1100

    First Commit

---
+++ repl.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Code for when running on windows */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* readline for windows */
+char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy-1] = '\0';
+    return cpy;
+    }
+
+/* add_history for windows */
+void add_history(char* unused) {}
+
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
+
+
+int main(int argc, char** argv) {
+
+    /* Print version and exit information */
+    puts("Lispy Version 0.0.0.1");
+    puts("Press Ctrl+c to Exit\n");
+
+    /* In a never eneding loop */
+    while (1) {
+        /* Output our prompt and get input*/
+        char* input = readline("lispy> ");
+
+        /* Add input to history */
+        add_history(input);
+
+        /* Echo inp[ut back to user */
+                printf("No you're a %s\n", input);
+
+        /* Free retrieved input */
+        free(input);
+
+    }
+    return 0;
+    }
+
+
+
+
+    
