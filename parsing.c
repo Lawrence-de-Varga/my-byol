bc95a0dc3f1ffe4577431f81507c04c053d1f85c
Lawrence de Varga <lawdevarga@gmail.com>
2023-01-09 21:06:10 +1100

    Chapter 7, added the eval and eval_op functions

--- parsing.c
+++ parsing.c
@@ -26,6 +26,37 @@ void add_history(char* unused) {}
 #include <editline/history.h>
 #endif
 
+long eval_op(long x, char* op, long y) {
+    if (strcmp(op, "+") == 0) {return x + y;}
+    if (strcmp(op, "-") == 0) {return x - y;}
+    if (strcmp(op, "*") == 0) {return x * y;}
+    if (strcmp(op, "/") == 0) {return x / y;}
+    return 0;
+}
+
+long eval(mpc_ast_t* t) {
+
+    /* If tagged as number return it directly */
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+
+    /* The operator is always the second child. */
+    char* op = t->children[1]->contents;
+
+    /* We store the third child in x. */
+    long x = eval(t->children[2]);
+
+    /* iterate over and combine the remaining childnre */
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+}
+
 
 int main(int argc, char** argv) {
     /* Create some parsers */
@@ -45,7 +76,7 @@ int main(int argc, char** argv) {
             Number, Operator, Expr, Lispy);
 
     /* Print version and exit information */
-    puts("Lispy Version 0.0.0.1");
+    puts("Lispy Version 0.0.0.3");
     puts("Press Ctrl+c to Exit\n");
 
     /* In a never eneding loop */
@@ -60,7 +91,8 @@ int main(int argc, char** argv) {
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r)) {
             /* On success print the AST */
-            mpc_ast_print(r.output);
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
         } else {
             /* Otherwise print the error */
@@ -78,6 +110,3 @@ int main(int argc, char** argv) {
     }
 
 
-
-
-    
